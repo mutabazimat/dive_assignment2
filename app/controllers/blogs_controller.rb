82d6dca827cdf68355aa4d499c3a9ae67b68ec1e
matthew <mutabazimg@gmail.com>
2018-02-28 17:52:09 +0000

    solving soem pdf issues

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -40,6 +40,6 @@ class BlogsController < ApplicationController
    
   def create
     Blog.create(title: params[:blog][:title], content: params[:blog][:content])
-    redirect_to "/blogs/index"
+    redirect_to "/"
   end
 end
